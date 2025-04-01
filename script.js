252b66f8d1a6901471fcde01f6d419a8ce15f9f7
Mohammad Adnan <65945728+adnandotdev@users.noreply.github.com>
2025-04-01 13:26:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 function trailingZeros(n) {
   //your JS code here. If required.
+	let factorial = 1
+	for(i=n;i>0;i--){
+		factorial *= i
+	}
+	let count = -1
+	while(Number.isInteger(factorial)){
+		factorial = factorial/10;
+		count++;
+	}
+	return count;
 }
-
 const input = prompt("Enter a number");
 alert(trailingZeros(input));
